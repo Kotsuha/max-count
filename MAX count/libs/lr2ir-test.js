b2e4748adee5a6bd165f02871f8e85be78ecf2a1
Hatsuko <inciesso@gmail.com>
2018-06-02 21:32:34 +0800

    Add lr2ir-test.js
    --HG--
    branch : lr2ir

---
+++ MAX count/libs/lr2ir-test.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const lr2ir = require("./lr2ir");
+
+// ↓↓ Please provide data ↓↓
+
+// A valid account
+const player = {
+	id: "",
+	password: ""
+};
+
+// Ids for testing rival addition
+const addRivalIds = [
+	"",
+	"",
+	"",
+];
+
+// Ids for testing rival deletion
+const deleteRivalIds = [
+	"",
+	"",
+	"",
+];
+
+// ↑↑ Please provide data ↑↑
+
+
