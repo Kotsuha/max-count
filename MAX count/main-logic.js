c7d5164ed085aea278fca91609075969a3c0be93
Hatsuko <inciesso@gmail.com>
2018-06-04 07:50:05 +0800

    Fix

--- MAX count/main-logic.js
+++ MAX count/main-logic.js
@@ -6,12 +6,13 @@ const server = require(__rootdir + "/server");
 
 function loginOrNot() {
 	return args.length === 2 
-		? lr2ir.setUser(args[0], args[1]).login() 
+		? lr2ir.setUser(args[0], args[1]).login().then(() => {
+			foon.log("LR2IR logged in", lr2ir.getUserCensored());
+		})
 		: Promise.resolve();
 }
 
 loginOrNot()
 .then(() => { 
-	foon.log("LR2IR logged in", lr2ir.getUserCensored());
 	server.run();
 });
