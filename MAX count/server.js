757e835fb1337cc1ac8e9fa3e892dcd3e27b7b38
Hatsuko <inciesso@gmail.com>
2018-06-04 08:06:41 +0800

    Add public/index.html

--- MAX count/server.js
+++ MAX count/server.js
@@ -4,12 +4,15 @@ const { __rootdir, foon } = require("./common");
 const CONFIG = require(__rootdir + "/config/config");
 
 const http = require("http");
+const express = require("express");
 
 module.exports.run = function() {
-	const server = http.createServer(function(request, response) {
-		response.writeHead(200);
-		response.end("Hello, World!");
-	});
+	const app = express();
+	const server = http.createServer(app);
+
+	const prefix = "",
+	      root = require("path").join(__rootdir, "public");
+	app.use(prefix, express.static(root));
 
 	server.listen(CONFIG.PORT, function() {
 		foon.log(`http://localhost:${CONFIG.PORT}`);
