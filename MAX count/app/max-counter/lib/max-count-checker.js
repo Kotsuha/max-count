d52c660c70928bda7f3377fd3dd97f53d4fb6838
Hatsuko <inciesso@gmail.com>
2018-06-22 02:45:36 +0800

    start
    --HG--
    branch : array-like

--- MAX count/app/max-counter/lib/max-count-checker.js
+++ MAX count/app/max-counter/lib/max-count-checker.js
@@ -1,30 +1,49 @@
 "use strict";
 
 const lr2ir = require("app/lr2ir");
-const MaxInfo = require("./model/max-info");
 const { MyList, SongRanking } = require("./util");
-const oopsRetry = require("app/oops-retry");
-const Promise = require("bluebird");
+const MaxInfo = require("./model/max-info");
 const wait = require("app/wait").dottedWait;
+const oopsRetry = require("app/oops-retry");
+
+const Promise = require("bluebird");
+Promise.config({
+	// Enable cancellation
+	cancellation: true,
+});
+const bluebird = Promise.resolve;
+
 
 const WAIT_MS = {
 	REQUEST: 3000,
 	RETRY: 10000,
 };
+
 const OOPS_NUM = {
 	RETRY: 2
 };
 
 const MaxCountChecker = function(lr2id, goal, report) {
 
-	this.checkMyList = (page) => lr2ir.openMyList(lr2id, "clear", page)
+	// act as the last check result
+	this.maxInfos = [];
+	this.push = function(maxInfo) {
+		this.maxInfos.push(maxInfo);
+	};
+	this.empty = function() {
+		this.maxInfos.length = 0;
+	};
+	this.isPassed = () => this.maxInfos.length >= goal;
+
+	this.checkMyList = (page) => bluebird()
+		.then(( ) => lr2ir.openMyList(lr2id, "clear", page))
 		.then(($) => {
-			const maxInfos = [];
+			this.empty();
 			const pdEntries = MyList.getPlayDataEntries($);
-			const isPassed = () => maxInfos.length >= goal;
+			
 
 			const collectMaxUntilPassed = (entry) => {
-				if (isPassed()) {
+				if (this.isPassed()) {
 					return Promise.resolve();
 				}
 
@@ -32,8 +51,8 @@ const MaxCountChecker = function(lr2id, goal, report) {
 				return this.autoRetry.checkSongRanking(entry.bmsid, "1")
 					.then((maxInfo) => {
 						if (maxInfo === undefined) return;
-						maxInfos.push(maxInfo);
-						report(maxInfo.score + " -> MAX: " + maxInfos.length + "\n");
+						this.push(maxInfo);
+						report(maxInfo.score + " -> MAX: " + this.maxInfos.length + "\n");
 					})
 					.then(() => wait(WAIT_MS.REQUEST))
 					.catch((err) => Promise.reject(err));
@@ -41,15 +60,15 @@ const MaxCountChecker = function(lr2id, goal, report) {
 
 			return Promise
 				.each(pdEntries, collectMaxUntilPassed)
-				.then(() => maxInfos)
+				.then(() => this.maxInfos)
 				.catch((err) => Promise.reject(err));
 		})
-		.catch(function(err) {
-			return Promise.reject(err);
-		});
+		.catch((err) => Promise.reject(err))
+		.finally(() => report(this.maxInfos));
 
-	this.checkSongRanking = (bmsid, page) => lr2ir.openSongRanking(bmsid, page)
-		.then(function($) {
+	this.checkSongRanking = (bmsid, page) => bluebird()
+		.then(( ) => lr2ir.openSongRanking(bmsid, page))
+		.then(($) => {
 			let maxInfo;
 			const rEntries = SongRanking.getRankingEntries($);
 			for (const entry of rEntries) {
@@ -62,13 +81,30 @@ const MaxCountChecker = function(lr2id, goal, report) {
 			}
 			return maxInfo;
 		})
-		.catch(function(err) {
-			return Promise.reject(err);
-		});
+		.catch((err) => Promise.reject(err));
 
 	this.autoRetry = {};
 	this.autoRetry.checkSongRanking = oopsRetry(
 		this.checkSongRanking, OOPS_NUM.RETRY, WAIT_MS.RETRY);
 };
 
+
 module.exports = MaxCountChecker;
+
+
+const logger = require("app/logger");
+const mcc = new MaxCountChecker("100746", 15, logger.log);
+let p = mcc.checkMyList(1)
+// mcc.checkSongRanking.autoRetry(10, 1)
+	.then(function(mi) {
+		logger.log(mi);
+	})
+	.catch(function(err) {
+		logger.log(err);
+	});
+
+wait(60000, "-")
+	.then(() => {
+		logger.log("cancel()");
+		p.cancel();
+	});
