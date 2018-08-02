d52a9ee3c33c5fba76acd6fd0369648a4078a741
Kotsuha <inciesso@gmail.com>
2018-08-02 16:02:24 +0800

    Add heroku-waker stuff to config.js

--- app/config/config.js
+++ app/config/config.js
@@ -6,6 +6,10 @@ const config = {
 	PORT: process.env.PORT || 3000,	// TODO?
 	LR2ID: process.env.LR2ID,		// TODO?
 	PASSWORD: process.env.PASSWORD,	// TODO?
+
+	// heroku-waker
+	APP_URL: process.env.APP_URL,									// TODO?
+	WAKE_INTERVAL: process.env.WAKE_INTERVAL || (29 * 60 * 1000),	// TODO?
 };
 
 module.exports = Object.freeze(config);
