060e3fe16e20283199ee43741a6d7d0cb2871589
Kotsuha <inciesso@gmail.com>
2018-08-02 15:43:15 +0800

    Fix

--- app/users.js
+++ app/users.js
@@ -4,20 +4,20 @@ const { __approot } = require("app/__global");
 const { MAX_USERS } = require(__approot + "/config/config");
 
 const users = (function() {
-	let current = 0;
-	// const isMaxReached = () => current >= MAX_USERS;
+	let curUsers = 0;
 	const change = function(num) {
-		const newUsers = current + num;
-		if (newUsers > MAX_USERS || newUsers < 0 || newUsers === current) {
+		const oldUsers = curUsers;
+		const newUsers = oldUsers + num;
+		if (newUsers > MAX_USERS || newUsers < 0 || newUsers === oldUsers) {
 			return false;
 		}
-		onUsersChange(newUsers);
-		if (current === 0 && newUsers > 0) {
-			onUsersBecomeGreaterThanZero(newUsers);
-		} else if (current > 0 && newUsers === 0) {
-			onUsersBecomeZero(newUsers);
+		curUsers = newUsers;
+		onUsersChange(curUsers);
+		if (oldUsers === 0 && curUsers > 0) {
+			onUsersBecomeGreaterThanZero(curUsers);
+		} else if (oldUsers > 0 && curUsers === 0) {
+			onUsersBecomeZero(curUsers);
 		}
-		current = newUsers;
 		return true;
 	};
 	const createEvent = require("app/event");
@@ -26,9 +26,8 @@ const users = (function() {
 	const onUsersBecomeZero            = createEvent();
 	const increase = () => change(+1);
 	const decrease = () => change(-1);
-	const toString = () => `${current}/${MAX_USERS}`;
+	const toString = () => `${curUsers}/${MAX_USERS}`;
 	return {
-		// isMaxReached,
 		increase,
 		decrease,
 		onUsersChange,
